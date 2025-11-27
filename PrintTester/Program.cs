137e1c5a0d1f3eefee71928990d076cea84b9176
NULLVOID76 <nullvoid76@gmail.com>
2025-11-27 15:47:12 +0530

    Add template

--- PrintTester/Program.cs
+++ PrintTester/Program.cs
@@ -1,21 +1,29 @@
 ﻿using System.Reflection;
-
+using System.Threading.Tasks;
+using LabelPrintHelper;
 namespace PrintTester
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             Console.WriteLine("Hello World");
 
+            IPrinterService printer = new PrinterService();
 
-            string template = "^XA ^FO50,50^ADN,36,20^FDName: {NAME}^FS ^FO50,100^ADN,36,20^FDDate: {DATE}^FS ^XZ";
+            string template = await File.ReadAllTextAsync("WarehouseTemplate.zpl");
 
 
             var parameters = new Dictionary<string, string>
             {
-                ["NAME"] = "Test Label",
-                ["DATE"] = DateTime.Now.ToString()
+                ["MODELNAME"] = "Redmi A3x/4G/4GB/64GB",
+                ["COLOR"] = "Midnight Black",
+                ["IDSKU"] = "MZB0I5AIN",
+                ["CBQTY"] = "480",
+                ["PALLET_NO"] = "P24048RN6CI5BA00014",
+                ["DATE"] = DateTime.Now.ToString("dd-MM-yyyy"),
+                ["TIME"] = DateTime.Now.ToString("HH:mm:ss"),
+                ["FGCODE"] = "9510C3Y2E0021"
             };
 
             string printerName = "";
@@ -23,11 +31,21 @@ namespace PrintTester
             printerName = Console.ReadLine();
 
 
-            
-            bool ok =Print.Printx(template, printerName);
+            try
+            {
+                //bool ok = printer.Print(template, printerName);
+                bool ok= await printer.PrintAsync(printerName, template, parameters);
+                //bool ok = printer.Print(printerName, template, parameters);
 
-            Console.WriteLine(ok ? "Printed successfully!" : "Print failed!");
-            Console.ReadLine();
+                Console.WriteLine(ok ? "Printed successfully!" : "Print failed!");
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Print failed!!!!");
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
